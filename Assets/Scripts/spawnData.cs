c00578d550e9f7135c1ffa880d56193eaa510604
Alexander Monaco <amona010@fiu.edu>
2018-10-20 15:44:25 -0400

    Alter practice files

--- Assets/Scripts/spawnData.cs
+++ Assets/Scripts/spawnData.cs
@@ -6,15 +6,18 @@ public class spawnData : MonoBehaviour {
 
     // Use this for initialization
     public GameObject dataType_general;
+    public GameObject otherDataType;
     public GameObject target;
 	void Start () {
-		
+        Instantiate(dataType_general);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
 
 
 }
