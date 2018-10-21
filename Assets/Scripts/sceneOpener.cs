b5e8333d210374198f3879ab316aeb93f5c88a05
Alexander Afanasyev <aa@cs.fiu.edu>
2018-10-21 15:50:32 -0400

    Add main screen with options

---
+++ Assets/Scripts/sceneOpener.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneOpener : MonoBehaviour
+{
+
+    public void
+    ShowFlow1()
+    {
+        SceneManager.LoadScene("Particle_Flow");
+    }
+
+}
