a9a450aa7c5d24ac5ae2014a3729bf9ef7d9fa9f
Alexander Monaco <32403661+amona010@users.noreply.github.com>
2018-10-21 15:23:50 -0400

    Added particle flow scene

---
+++ Assets/Scripts/JSONFlowDataHelper.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JSONFlowDataHelper : MonoBehaviour {
+
+    private JSONDataHandler handler;
+    public GameObject hostObj;
+    public GameObject flowObj;
+    public GameObject[] hosts;
+
+    private void Awake()
+    {
+        handler = gameObject.GetComponent<JSONDataHandler>();
+        getAllHosts();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+	}
+
+    void getAllHosts()
+    {
+        handler.readFromJSON();
+        Debug.Log("Success");
+        foreach(projData.host host in handler.data.hosts)
+        {
+            GameObject newHost = Instantiate(hostObj);
+            newHost.name = host.name;
+            newHost.transform.position = new Vector3(host.location.x, host.location.y, host.location.z);
+            newHost.transform.parent = flowObj.transform;
+        }
+    }
+
+    void saveData()
+    {
+        foreach(GameObject obj in hosts)
+        {
+            handler.addNewHost(obj.name, obj.transform.position.x, obj.transform.position.y, obj.transform.position.z);
+        }
+
+        handler.saveToJSON();
+    }
+}
